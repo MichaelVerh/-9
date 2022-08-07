e612ff170367b90094d4468361e18f4e10b33576
Michael <wermian@mail.ru>
2022-08-07 17:58:47 +0300

    Init

---
+++ Program-HW-T1.cs
@@ -0,0 +1,14 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N, кратные трём.
+Console.Write("Введите M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"N={N}, M={M}");
+if (N>M)
+for (int i = M; i <= N; i++)
+    if (i%3==0)
+    Console.Write($" {i}");
+else
+    for (int j = N; j <= M; j++)
+        if (j%3==0)
+        Console.Write($" {j}");
